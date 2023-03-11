4002fdeee87610da953ab8fed7aed510ec30fe52
Самаренко Андрей Валерьевич <a.samarenko@tinkoff.ru>
2023-03-11 11:34:15 +0300

    Инициализация проекта

---
+++ BricsScoial/BricsScoial/BricsScoialApp.swift
@@ -0,0 +1,17 @@
+//
+//  BricsScoialApp.swift
+//  BricsScoial
+//
+//  Created by Samarenko Andrey on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct BricsScoialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
